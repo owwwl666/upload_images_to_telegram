304d6c70151de201621756885444bd5314d51618
owwwl666 <zaharsalam228@gmail.com>
2023-06-08 11:44:26 +1000

    Create fetch_nasa_epic_images.py
    Download photos from NASA_EPIC

---
+++ fetch_nasa_epic_images.py
@@ -0,0 +1,31 @@
+import requests
+import pathlib
+import argparse
+from datetime import datetime
+
+
+def downloads_images_from_nasa_epic(images_folder='./images', api_key='DEMO_KEY'):
+    pathlib.Path(images_folder).mkdir(parents=True, exist_ok=True)
+    epic_url = requests.get(url='https://api.nasa.gov/EPIC/api/natural',
+                            params={'api_key': api_key}).json()[:3]
+    for image in epic_url:
+        image_date = datetime.fromisoformat(image.get('date')) \
+            .strftime('%Y/%m/%d')
+        image_name = image.get('image')
+        image_id = image.get('identifier')
+        epic_image_url = f'https://api.nasa.gov/EPIC/archive/natural/' \
+                         f'{image_date}/png/{image_name}.png'
+        response = requests.get(epic_image_url, params={'api_key': api_key})
+        with open(f'{images_folder}/epic_{image_id}.png', 'wb') as file:
+            file.write(response.content)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("api_key")
+    args = parser.parse_args()
+    downloads_images_from_nasa_epic(api_key=args.api_key)
+
+
+if __name__ == '__main__':
+    main()
