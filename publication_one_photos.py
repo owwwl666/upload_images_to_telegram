ffd13b1e8623936ea86fa25893dde6a3db16f89b
owwwl666 <zaharsalam228@gmail.com>
2023-06-08 23:03:23 +1000

    Update publication_one_photos.py
    Added ifname

--- publication_one_photos.py
+++ publication_one_photos.py
@@ -3,16 +3,22 @@ import random
 import telegram
 from environs import Env
 
-env = Env()
-env.read_env()
 
-bot = telegram.Bot(token=env('TELEGRAM_BOT_TOKEN'))
+def main():
+    env = Env()
+    env.read_env()
 
-photos = os.listdir('./images')
+    bot = telegram.Bot(token=env('TELEGRAM_BOT_TOKEN'))
 
-try:
-    bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(env('PATH_TO_PHOTOS'), 'rb'))
-except:
-    random.shuffle(photos)
-    photo = photos[0]
-    bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(f'./images/{photo}', 'rb'))
+    photos = os.listdir('./images')
+
+    try:
+        bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(env('PATH_TO_PHOTOS'), 'rb'))
+    except:
+        random.shuffle(photos)
+        photo = photos[0]
+        bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(f'./images/{photo}', 'rb'))
+
+
+if __name__ == '__main__':
+    main()
