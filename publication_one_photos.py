1fa690743b15cfa7bd79c2b30b2e44f4d1b76511
owwwl666 <zaharsalam228@gmail.com>
2023-06-09 21:45:07 +1000

    Update publication_one_photos.py
    added an environment variable to set the path to the photo directory

--- publication_one_photos.py
+++ publication_one_photos.py
@@ -9,13 +9,13 @@ def main():
     env.read_env()
 
     bot = telegram.Bot(token=env('TELEGRAM_BOT_TOKEN'))
-
+    directory = env('PATH_TO_PHOTOS_DIRECTORY')
     photos = os.listdir('./images')
 
     try:
         bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(env('PATH_TO_PHOTOS'), 'rb'))
     except:
-        bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(f'./images/{random.choice(photos)}', 'rb'))
+        bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int), photo=open(f'{directory}{random.choice(photos)}', 'rb'))
 
 
 if __name__ == '__main__':
