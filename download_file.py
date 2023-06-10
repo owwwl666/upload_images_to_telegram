4e86d5d17c6f3d8ccaba4658c79d046fd1b5b832
owwwl666 <zaharsalam228@gmail.com>
2023-06-11 02:28:34 +1000

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -6,5 +6,5 @@ def downloads_file(url, folder, file_name, **kwargs):
     """Скачивание и сохранение файла"""
     response = requests.get(url, params=kwargs)
     response.raise_for_status()
-    with open(f'{Path(folder).joinpath(file_name)}', 'wb') as file:
+    with open(Path(folder).joinpath(file_name), 'wb') as file:
         file.write(response.content)
