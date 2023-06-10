29cca75a3b3e6511ca8ee74726bcce0ecb89da8d
owwwl666 <zaharsalam228@gmail.com>
2023-06-11 02:27:12 +1000

    Update fetch_nasa_epic_images.py
    changed the name of the variable

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -6,12 +6,12 @@ from download_file import downloads_file
 
 
 def downloads_images_from_nasa_epic(images_folder=pathlib.Path.cwd().joinpath('images'),
-                                    api_key='DEMO_KEY', quantity_photos=3):
+                                    api_key='DEMO_KEY', photos_quantity=3):
     pathlib.Path(images_folder).mkdir(parents=True, exist_ok=True)
     earth_photos_url = requests.get(url='https://api.nasa.gov/EPIC/api/natural',
                                     params={'api_key': api_key})
     earth_photos_url.raise_for_status()
-    earth_photos = earth_photos_url.json()[:quantity_photos]
+    earth_photos = earth_photos_url.json()[:photos_quantity]
 
     for image in earth_photos:
         image_date = datetime.fromisoformat(image['date']) \
@@ -27,9 +27,9 @@ def downloads_images_from_nasa_epic(images_folder=pathlib.Path.cwd().joinpath('i
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-key", "--api_key", required=False, default='DEMO_KEY')
-    parser.add_argument("-qp", "--quantity_photos", required=False, default=3, type=int)
+    parser.add_argument("-qp", "--photos_quantity", required=False, default=3, type=int)
     args = parser.parse_args()
-    downloads_images_from_nasa_epic(api_key=args.api_key, quantity_photos=args.quantity_photos)
+    downloads_images_from_nasa_epic(api_key=args.api_key, photos_quantity=args.photos_quantity)
 
 
 if __name__ == '__main__':
