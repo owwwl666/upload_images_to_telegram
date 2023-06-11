d29a1d57549d6700aa7189c3c661335710df9a17
owwwl666 <zaharsalam228@gmail.com>
2023-06-12 01:21:30 +1000

    Update publication_one_photos.py

--- publication_all_photos.py
+++ publication_all_photos.py
@@ -13,8 +13,9 @@ def main():
     env.read_env()
 
     bot = telegram.Bot(token=env('TELEGRAM_BOT_TOKEN'))
-    directory = pathlib.Path.cwd().joinpath(env('PATH_TO_PHOTOS_DIRECTORY'))
-    photos = os.listdir(pathlib.Path.cwd().joinpath(env('PATH_TO_PHOTOS_DIRECTORY')))
+    chat_id = env('TELEGRAM_CHAT_ID')
+    directory = pathlib.Path.cwd().joinpath(env('PATH_TO_PHOTOS_DIRECTORY', './images'))
+    photos = os.listdir(pathlib.Path.cwd().joinpath(env('PATH_TO_PHOTOS_DIRECTORY', './images')))
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-pause", "--publication_pause", required=False, default=4, type=int)
@@ -25,9 +26,9 @@ def main():
         try:
             random.shuffle(photos)
             for photo in photos:
-                bot.send_photo(chat_id=env('TELEGRAM_CHAT_ID', int),
+                bot.send_photo(chat_id=chat_id,
                                photo=reads_file(pathlib.Path(directory).joinpath(photo)))
-                time.sleep(3600*publication_pause)
+                time.sleep(3600 * publication_pause)
         except telegram.error.NetworkError:
             time.sleep(30)
             continue
